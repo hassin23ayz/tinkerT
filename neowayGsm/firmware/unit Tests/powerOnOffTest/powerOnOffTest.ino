50b27f3c9894fd21cd53ef2336b37f5948203430
ayz <hassin23ayz@gmail.com>
2016-12-03 01:32:46 +0600

    neoway AT comand listed for mqtt

--- neowayGsm/firmware/unit Tests/powerOnOffTest/powerOnOffTest.ino
+++ neowayGsm/firmware/unit Tests/powerOnOffTest/powerOnOffTest.ino
@@ -4,26 +4,32 @@ SoftwareSerial debug(5,6); // RX[MOSI], TX[MISO]
 
 #define LED_RED 14
 #define LED_GREEN 15
-#define REG_EN_PIN 3
-#define GSM_SW_PIN 2
+
+#define REG_EN_PIN 3    //active high
+#define GSM_SW_PIN 2    //active Low(~)
+
+#define SIZE_NEOWAY_OK 3
+#define SIZE_RESPONSE 64
+
 
 void pinsInit(void)
 {
     pinMode(REG_EN_PIN, OUTPUT);
     pinMode(GSM_SW_PIN, OUTPUT);
     //giving the inactive stages at first
-    digitalWrite(GSM_SW_PIN, LOW);   //active low(~)  //gsm SW on off pin is at high now 
-    digitalWrite(REG_EN_PIN, LOW);   //active high  //regulator enable pin is at low noe
+    digitalWrite(GSM_SW_PIN, LOW);   //gsm SW on off pin is at high now as it is internally pulled up
+    digitalWrite(REG_EN_PIN, LOW);   //regulator enable pin is at low noe
     delay(500);                      //for safety
 }
 void powerOn(void)
 {
     digitalWrite(REG_EN_PIN, HIGH);  //power the module by enabling the regulator
     delay(100);                      //wait 100ms for stability of power reach
-    digitalWrite(GSM_SW_PIN,HIGH);   //gsm SW on off pin is pulled down 
-    delay(500);                      //for 500ms 
-    digitalWrite(GSM_SW_PIN, LOW);   //gsm SW on off pin is pulled up again [by nternal resistor of modem]
+    digitalWrite(GSM_SW_PIN,HIGH);   //gsm SW on off pin is now made Low 
+    delay(500);                      //keep the pin Low for 500ms 
+    digitalWrite(GSM_SW_PIN, LOW);   //make gsm SW on off pin pulled up again 
 
+    //Now neoway gsm module should power up now and +PBREADY type msg should come from neoway
     digitalWrite(LED_RED,LOW);      //LED notification
     digitalWrite(LED_GREEN,HIGH);
 }
@@ -40,9 +46,9 @@ void powerOff(void)
    digitalWrite(LED_RED,HIGH);  
 }
 
-uint8_t findSubString(char* mainStr, uint8_t mainLen, char* subStr, uint8_t subLen)
+bool findSubString(char* mainStr, uint8_t mainLen, char* subStr, uint8_t subLen)
 {  
-  uint8_t ans = false;
+  bool ans = false;
   uint8_t count = 0;
   for(uint8_t i=0; i<mainLen; i++)
   {
@@ -59,21 +65,55 @@ uint8_t findSubString(char* mainStr, uint8_t mainLen, char* subStr, uint8_t subL
   return ans; 
 }
 
+bool sendATcommand(char* ATcommand, unsigned long timeout)
+{  
+  uint8_t n=0;
+  bool ans = false;
+  char response[SIZE_RESPONSE];
+  memset(response,'\0',SIZE_RESPONSE);
+  unsigned long previous = 0;
+  
+  n=0;  
+  previous = millis();
+  Serial1.flush();               
+  Serial1.println(ATcommand);    // Send the AT command
+  do                             // this loop waits for the answer until timeouts
+  {
+    if(Serial1.available() > 0)
+    {
+      char c = Serial1.read();
+      if (c != -1)
+      {
+        response[n] = c;
+        n = ((n+1) % SIZE_RESPONSE);
+        if (findSubString((char*)response,SIZE_RESPONSE, "OK", SIZE_NEOWAY_OK))
+        {
+          ans = true;
+          break;
+        }
+      }
+    }
+  }
+  while((unsigned long)(millis() - previous) < timeout); 
+  return ans;  
+}
+
+
+
 void setup() 
 {
-  debug.begin(9600);
+  debug.begin(19200);
   Serial1.begin(19200);
   
   pinMode(LED_GREEN,OUTPUT);
   pinMode(LED_RED, OUTPUT);
-  debug.println("initializing pins for Gsm module power control");
   pinsInit();
+  debug.println("Gsm module power control pins initialized");
 }
 
 void loop() 
 { 
-  /*
-   * //ideal for sending AT commands to gsm Module directly
+  //ideal for sending AT commands to gsm Module directly
   if (debug.available()) 
   {
       Serial1.write(debug.read());
@@ -81,49 +121,28 @@ void loop()
   if (Serial1.available()) 
   {
     debug.write(Serial1.read());
-  }*/
+  }
   if(debug.available()>0)
   {
-    if(debug.read()=='1')
+    char ch = (char)debug.read();
+    if(ch=='1')
     {
-      debug.print("you said: powerOnModule");
+      debug.println("you said: powerOnModule");
       powerOn();
     }
-    else if(debug.read()=='0')
+    else if(ch=='0')
     {
-      debug.print("you said: powerOffModule");
+      debug.println("you said: powerOffModule");
       powerOff();       
     }
-    else if(debug.read()=='6')
+    else if(ch=='6')
     {
-      debug.print("you queried: moduleStatus");
-      Serial1.println("AT");
-      delay(10);
-      if(Serial1.available()>0)
-      {
-        char neowayReply[64];
-        memset(neowayReply, '\0', 64);
-        uint8_t i = 0; 
-        while(Serial1.available()>0)
-        {
-          neowayReply[i++] = Serial1.read();
-        }//fill incoming buffer
-
-        if(findSubString(neowayReply, 128, "OK", 3))
-        {
-          debug.print("MODULE IS OK");
-        }
-        else
-        {
-          debug.print("could not understand what module said");
-          digitalWrite(LED_GREEN,LOW);
-          digitalWrite(LED_RED,HIGH);  
-        }
-      }//if serial available
+      debug.println("you queried: moduleStatus");
+      bool isModuleOn = sendATcommand("AT", 2000);
+      if(isModuleOn)
+        debug.println("neoway is powered On");
       else
-      {
-        debug.print("module did not answer in 10ms");
-      }
+        debug.println("neoway Off");           
     }//if query cmd 6 came
   }//if any command got from debug port
 }
