6318c024780c2202270230874fb6a541f97f808a
ayz03 <hassin23ayz@gmail.com>
2016-12-11 18:38:59 +0600

    platformio eclipse going on

---
+++ Alternatives Of Arduino IDE/Using Eclipse & PlatformIO/blinkOnUno/src/main.cpp
@@ -0,0 +1,11 @@
+void setup()
+{
+  /************************** pointer ***********************************/
+  Serial.begin(9600);
+  Serial.println("hi i am eclipse you look like an alien");
+}
+
+void loop()
+{
+
+}
